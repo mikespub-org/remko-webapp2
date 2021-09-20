b668036b90ccb1f240490a82d4c438c7a60b1db5
mikespub <github@mikespub.net>
2021-09-20 16:25:40 +0000

    recompile template1

--- tests/resources/jinja2_templates_compiled/tmpl_3a79873b1b49be244fd5444b1258ce348be26de8.py
+++ tests/resources/jinja2_templates_compiled/tmpl_3a79873b1b49be244fd5444b1258ce348be26de8.py
@@ -1,15 +1,15 @@
-from jinja2.runtime import to_string
-
-
+from __future__ import generator_stop
+from jinja2.runtime import LoopContext, Macro, Markup, Namespace, TemplateNotFound, TemplateReference, TemplateRuntimeError, Undefined, concat, escape, identity, internalcode, markup_join, missing, str_join
 name = 'template1.html'
 
-
-def root(context):
-    l_message = context.resolve('message')
-    if 0:
-        yield None
-    yield to_string(l_message)
-
+def root(context, missing=missing):
+    resolve = context.resolve_or_missing
+    undefined = environment.undefined
+    cond_expr_undefined = Undefined
+    if 0: yield None
+    l_0_message = resolve('message')
+    pass
+    yield str(undefined(name='message') if l_0_message is missing else l_0_message)
 
 blocks = {}
-debug_info = '1=8'
+debug_info = '1=12'
